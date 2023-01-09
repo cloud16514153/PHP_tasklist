84ed25e551c4fd8f6129f34244d674ba93420506
cloud16514153 <jitsukeweb@gmail.com>
2023-01-09 05:29:09 +0000

    tasklist

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -15,13 +15,23 @@ class TasksController extends Controller
      */
     public function index()
     {
-        // メッセージ一覧を取得
-        $tasks = Task::orderBy('id', 'desc')->paginate(25);
+        $data = [];
+        if (\Auth::check()) { // 認証済みの場合
+            // 認証済みユーザを取得
+            $user = \Auth::user();
+            // ユーザの投稿の一覧を作成日時の降順で取得
+            // （後のChapterで他ユーザの投稿も取得するように変更しますが、現時点ではこのユーザの投稿のみ取得します）
+            $tasks = $user->tasks()->orderBy('created_at', 'desc')->paginate(10);
+            $data = [
+                    'user' => $user,
+                    'tasks' => $tasks,
+            ];
+        }
+
 
         // メッセージ一覧ビューでそれを表示
         return view('tasks.index', [
-            'tasks' => $tasks,
-            ]);
+            'tasks' => $tasks, ]);
     }
 
     /**
@@ -53,15 +63,14 @@ class TasksController extends Controller
             'content' => 'required|max:255',
         ]);
         
-        // メッセージを作成
-        $task = new Task;
-        $task->status = $request->status;
-        $task->content = $request->content;
-        $task->save();
+        /// 認証済みユーザ（閲覧者）の投稿として作成（リクエストされた値をもとに作成）
+        $request->user()->tasks()->create([
+            'status' => $request->status,
+            'content' => $request->content,
+        ]);
 
         // トップページへリダイレクトさせる
         return redirect('/');
-
     }
 
     /**
@@ -72,13 +81,20 @@ class TasksController extends Controller
      */
     public function show($id)
     {
+        
         // idの値でメッセージを検索して取得
         $task = Task::findOrFail($id);
 
         // メッセージ詳細ビューでそれを表示
+        if (\Auth::id() === $task->user_id) {
         return view('tasks.show', [
             'task' => $task,
         ]);
+                // トップページへリダイレクトさせる
+
+        } else {
+        return redirect('/');
+        }
     }
 
     /**
@@ -93,9 +109,13 @@ class TasksController extends Controller
         $task = Task::findOrFail($id);
 
         // メッセージ編集ビューでそれを表示
+        if (\Auth::id() === $task->user_id) {
         return view('tasks.edit', [
             'task' => $task,
         ]);
+        } else {
+        return redirect('/');
+        }
     }
 
     /**
@@ -117,11 +137,13 @@ class TasksController extends Controller
         // idの値でメッセージを検索して取得
         $task = Task::findOrFail($id);
         // メッセージを更新
+        if (\Auth::id() === $task->user_id) {
         $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
         // トップページへリダイレクトさせる
+        }
         return redirect('/');
     }
 
